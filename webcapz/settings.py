838d3f86d32dc185f8f7d93b64bdeab14830ad18
Chekwube Utomi <chekwubeutomi@gmail.com>
2024-07-17 16:56:56 +0100

    .env added

--- webcapz/settings.py
+++ webcapz/settings.py
@@ -23,7 +23,7 @@ load_dotenv()
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(vi3jl4@@lzfd@u1syrr4n_w=1@2j155hph^o@90d6m!cxab!m'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
