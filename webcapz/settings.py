8ec79bf9b946c3909189ef509e183bc3309a0e14
Chekwube Utomi <chekwubeutomi@gmail.com>
2024-07-17 14:43:50 +0100

    initial commit

--- webcapz/settings.py
+++ webcapz/settings.py
@@ -79,6 +79,12 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        # 'USER':
+        # 'PASSWORD':
+        # 'HOST':
+        # 'PORT': 
     }
 }
 
