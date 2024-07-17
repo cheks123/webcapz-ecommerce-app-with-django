554aaa022a5484b45bd1286b7ff10ba4498977b6
Chekwube Utomi <chekwubeutomi@gmail.com>
2024-07-17 16:52:31 +0100

    postgres added

--- webcapz/settings.py
+++ webcapz/settings.py
@@ -11,10 +11,13 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -77,14 +80,14 @@ WSGI_APPLICATION = 'webcapz.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': BASE_DIR / 'db.sqlite3',
-        # 'USER':
-        # 'PASSWORD':
-        # 'HOST':
-        # 'PORT': 
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': os.environ.get("DB_PASSWORD"),
+        'HOST': 'roundhouse.proxy.rlwy.net',
+        'PORT': '14301',
     }
 }
 
